8284b8d41ae7e94653c8c068c4281233d59c9d21
Richard Shackleton <shackleberry112@gmail.com>
2023-01-14 19:41:28 +0000

    try fix elsewhere

--- adaptors/vercel-edge/vite.config.ts
+++ adaptors/vercel-edge/vite.config.ts
@@ -2,9 +2,6 @@ import { vercelEdgeAdaptor } from '@builder.io/qwik-city/adaptors/vercel-edge/vi
 import { extendConfig } from '@builder.io/qwik-city/vite';
 import baseConfig from '../../vite.config';
 
-// remove once this has been released: https://github.com/BuilderIO/qwik/issues/2597
-(String as any).prototype.toUppercase = String.prototype.toUpperCase;
-
 export default extendConfig(baseConfig, () => {
   return {
     build: {
