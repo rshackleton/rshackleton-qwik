a611e5c888a6ce67588c8ed521f8d7f3f5b95cc1
Richard Shackleton <shackleberry112@gmail.com>
2023-01-15 17:49:33 +0000

    add article schema

---
+++ packages/studio/schemas/article.ts
@@ -0,0 +1,19 @@
+import { SchemaTypeDefinition } from 'sanity';
+
+export default {
+  name: 'article',
+  title: 'Article',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+    },
+  ],
+} satisfies SchemaTypeDefinition;
