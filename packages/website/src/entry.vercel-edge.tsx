1119ffd05cb99cbbfbf03f2817d4efacf4cc36dc
Richard Shackleton <shackleberry112@gmail.com>
2023-01-16 00:46:17 +0000

    add temp fix for uppercase bug in qwik-city beta

--- packages/website/src/entry.vercel-edge.tsx
+++ packages/website/src/entry.vercel-edge.tsx
@@ -2,4 +2,7 @@ import { createQwikCity } from '@builder.io/qwik-city/middleware/vercel-edge';
 import qwikCityPlan from '@qwik-city-plan';
 import render from './entry.ssr';
 
+// remove once this has been released: https://github.com/BuilderIO/qwik/issues/2597
+(String as any).prototype.toUppercase = String.prototype.toUpperCase;
+
 export default createQwikCity({ render, qwikCityPlan });
