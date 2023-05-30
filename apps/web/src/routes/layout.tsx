0c73ffb3d326bf6ec8e7e902615b2bd8c3e1a622
Richard Shackleton <shackleberry112@gmail.com>
2023-05-30 13:28:15 +0100

    feat: add caching

--- apps/web/src/routes/layout.tsx
+++ apps/web/src/routes/layout.tsx
@@ -1,5 +1,15 @@
 import { component$, Slot } from '@builder.io/qwik';
+import type { RequestHandler } from "@builder.io/qwik-city";
 import { Footer, Header } from 'ui';
+
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+  cacheControl({
+    // Always serve a cached response by default, up to a week stale
+    staleWhileRevalidate: 60 * 60 * 24 * 7,
+    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+    maxAge: 5,
+  });
+};
 
 export default component$(() => {
   return (
